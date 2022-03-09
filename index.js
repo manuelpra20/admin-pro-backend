b8284ea0c2999648f0670ccf00ff895d3a6c54a6
manuelpra20 <manuelpra@gmail.com>
2022-03-09 00:22:14 -0400

    fin de seccion 10 -Auth

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-
-
-
-
 require('dotenv').config();
-const  express = require('express');
+
+const express = require('express');
 const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
-// crear servidor express
+// Crear el servidor de express
 const app = express();
 
 // Configurar CORS
 app.use( cors() );
 
+// Lectura y parseo del body (para hacer prueba y pasarle datos desde el postman )
+app.use( express.json() );
 
 // Base de datos
 dbConnection();
 
 
-
 // Rutas
-app.get( '/', (req, res) => {
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-
-});
 
 
 app.listen( process.env.PORT, () => {
